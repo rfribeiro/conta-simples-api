6b7737bdbd7b221f97d134e1ea76b0a1089bb6e8
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-21 23:06:42 -0300

    add Enterprise controller

---
+++ backend/src/controllers/EnterpriseController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+class EnterpriseController {
+
+    async register(req: Request, res: Response) {
+
+    }
+}
+
+export default new EnterpriseController()
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 
 import AuthController from './controllers/AuthController'
+import EnterpriseController from './controllers/EnterpriseController'
 
 const router = Router()
 
+// Authentication endpoints
 router.post('/login', AuthController.authenticate)
+
+// Enterprise endpoints
+router.post('/register', EnterpriseController.register)
 
 export default router
