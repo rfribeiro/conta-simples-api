853f7de208b2ddbc33643797d440dc602cec6138
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-22 19:24:24 -0300

    implement enterprise register method

--- backend/src/app/controllers/EnterpriseController.ts
+++ backend/src/app/controllers/EnterpriseController.ts
@@ -1,9 +1,64 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import BankAccount from '../models/BankAccount';
+import Enterprise from '../models/Enterprise';
+import User from '../models/User';
+
 
 class EnterpriseController {
 
-    async register(req: Request, res: Response) {
+    async index(req: Request, res: Response) {
+        const { userId } = req
 
+    }
+
+    async show(req: Request, res: Response) {
+        const { userId } = req
+        const { id } = req.params;
+
+        const enterpriseRepository = getRepository(Enterprise);
+
+        const enterprise = await enterpriseRepository.findOneOrFail(id, {
+            relations: ['bank']
+        })
+
+        return res.send(enterprise)
+    }
+
+    async register(req: Request, res: Response) {
+        try 
+        {
+            const { userId } = req
+            const { name, cnpj } = req.body
+
+            const repositoryEnterprise = getRepository(Enterprise);
+            const repositoryUser = getRepository(User);
+
+            const enterpriseExists = await repositoryEnterprise.findOne({ where: { cnpj }})
+
+            if (enterpriseExists) {
+                return res.sendStatus(409)
+            }
+
+            const user = await repositoryUser.findOneOrFail(userId)
+
+            const bankAccount = new BankAccount()
+            bankAccount.bankName = "CONTA SIMPLES"
+            bankAccount.bankNumber = 9999
+            bankAccount.balance = 0
+            bankAccount.agency = 1
+
+            const enterprise = repositoryEnterprise.create({ name, cnpj, bankAccount, user})
+            await repositoryEnterprise.save(enterprise)
+
+            return res.status(201).json({
+                enterprise
+            })
+
+        } catch (err) {
+            console.log(err)
+            return res.status(400).send({ error: 'Cannot register user, try again' })
+        }
     }
 }
 
