5676d135e3333f1ad5b5eeaddf7cd279f3e8d616
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-22 00:02:23 -0300

    add authenticate method

--- backend/src/app/controllers/AuthController.ts
+++ backend/src/app/controllers/AuthController.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import bcrypt from 'bcryptjs'
 
 class AuthController {
     async authenticate(req: Request, res: Response) {
         try {
-            return res.send('OK')
+            const repository = getRepository(User)
+
+            const { email, password } = req.body
+
+            const user = await repository.findOne({ where: { email }})
+
+            if (!user) {
+                return res.sendStatus(400)
+            }
+
+            const isValidPassword = await bcrypt.compare(password, user.password)
+
+            if (!isValidPassword) {
+                return res.sendStatus(401)
+            }
+
+            delete user.password
+            delete user.passwordResetToken
+            delete user.passwordResetExpires
+
+            return res.json({
+                user,
+            })
         } catch (err) {
             res.status(400).send({ error: 'Cannot authenticate user, try again' })
         }
