3ee0fafe9207e9887b91b8c39560ad284176deea
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-23 01:11:11 -0300

    change file name to merge pattern

--- backend/src/app/views/transaction_view.ts
+++ backend/src/app/views/transaction_view.ts
@@ -5,10 +5,11 @@ import TransactionTypeView from './transactiontype_view'
 class TransactionView {
 
     render(data: Transaction) {
-        const { id, createdAt, value, local, credit } = data
+        const { id, createdAt, value, local, credit, enterprise } = data
         const { type, description } = TransactionTypeView.render(data.type)
         return ({
             id,
+            enterpriseId: enterprise.id,
             createdAt,
             value,
             type,
