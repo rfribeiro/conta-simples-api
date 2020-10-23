09a01af018be17e14ac01637fece5f60a5f11ed5
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-22 22:09:22 -0300

    add transaction type create method

--- backend/src/app/controllers/TransactionTypeController.ts
+++ backend/src/app/controllers/TransactionTypeController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
-
+import { getRepository } from 'typeorm';
+import TransactionType from '../models/TransactionType';
+import TransactionTypeView from '../views/transactiontype_view';
 
 class TransactionTypeController {
     async index(req: Request, res: Response) {
@@ -7,7 +9,27 @@ class TransactionTypeController {
     }
 
     async create(req: Request, res: Response) {
-        return res.send('OK');
+        try 
+        {
+            const repository = getRepository(TransactionType)
+
+            const { type, description} = req.body
+
+            const exists = await repository.findOne({ where: { type }})
+
+            if (exists) {
+                return res.sendStatus(409)
+            }
+
+            const typeDesc = repository.create({ type, description })
+            await repository.save(typeDesc)
+
+            return res.status(201).json(
+                TransactionTypeView.render(typeDesc),
+            )
+        } catch (err) {
+            return res.status(400).send({ error: 'Cannot register user, try again' })
+        }
     }
 }
 
