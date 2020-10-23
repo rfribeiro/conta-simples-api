b6a34c00686561520f7adb5bc89c8c834f74dfa6
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-23 01:24:52 -0300

    fix token test after changes

--- backend/src/app/tests/utils/TokenHelper.test.ts
+++ backend/src/app/tests/utils/TokenHelper.test.ts
@@ -5,19 +5,18 @@ const ID = 'rafael@32123';
 
 describe.only('TokenHelper test suite', function () {
     it('deve gerar um hash a partir de uma senha', async () => {
-        const result = await TokenHelper.generate({id : ID});
+        const result = await TokenHelper.generate({userId : ID});
         assert.ok(result.length > 10);
     });
     it('verificar o reset token', async () => {
         const result = await TokenHelper.generateReset();
-        console.log(result)
         assert.ok(result.length === 20*2);
     });
     it('verificar o token recebido', async () => {
-        const resultToken = await TokenHelper.generate({id : ID});
+        const resultToken = await TokenHelper.generate({userId : ID});
 
         const resultVerify = await TokenHelper.verify(resultToken);
 
-        assert.equal(resultVerify.id, ID)
+        assert.equal(resultVerify.userId, ID)
     });
 });
