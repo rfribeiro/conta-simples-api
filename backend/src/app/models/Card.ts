f9edf87e0ad96ffa39af341c3703a2a0488364eb
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-24 18:07:53 -0300

    add balance and transactions relation on Card model

--- backend/src/app/models/Card.ts
+++ backend/src/app/models/Card.ts
@@ -1,5 +1,7 @@
-import { Column, ManyToOne, JoinColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
+require('dotenv/config')
+import { Column, ManyToOne, JoinColumn, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
 import Enterprise from './Enterprise';
+import Transaction from './Transaction';
 
 @Entity('cards')
 class Card {
@@ -9,9 +11,19 @@ class Card {
     @Column()
     number: string;
 
+    @Column({ default: 0 })
+    balance: number;
+
     @ManyToOne(type => Enterprise)
     @JoinColumn({name: 'enterprise_id'})
     enterprise: Enterprise;
+
+    @OneToMany(type => Transaction, transaction => transaction.card)
+    transactions: Transaction[];
+
+    final() {
+        return this.number.substr(this.number.length - Number(process.env.CARD_FINAL_LENGHT))
+    }
 }
 
 export default Card;
