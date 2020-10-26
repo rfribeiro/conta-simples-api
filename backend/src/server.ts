d55a91be461ac060f1985d325ed34b661e45bdae
Rafael Fernando Ribeiro <rfribeiro@gmail.com>
2020-10-25 22:17:40 -0300

    add error handle on 404

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,4 +7,11 @@ const app = express()
 app.use(express.json())
 app.use('/api/v1', router)
 
+//Capture All 404 errors
+app.use(function (req,res,next){
+	res.status(404).send({
+        error: 'Unable to find the requested resource!'
+    });
+});
+
 app.listen(3000, () => console.log('🔥 Server started'));
